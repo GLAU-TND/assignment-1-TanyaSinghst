db99b80413fa8c6e510eb5248d5fe34d52d49146
TanyaSinghst <tanya.singh_iot18@gla.ac.in>
2020-02-24 17:40:00 +0530

    constructor for person class created

--- src/defination/Person.java
+++ src/defination/Person.java
@@ -1,4 +1,17 @@
 package defination;
 
-public class Person {
+import java.io.Serializable;
+
+public class Person implements Serializable {
+    private String firstName;
+    private String lastName;
+    private String contactNumber;
+    private String anotherContactNumber;
+    private String emailAddress;
+    private Person next;
+
+    public Person() {
+        next = null;
+    }
+
 }
